d48a0103728fb0c1ec2aaeac84ab2566619a9d2e
Omar Corchado <omar@timb3r.io>
2023-10-19 22:10:58 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Code Challenge: Capital Gains
+
+Code challenge For Nu By Omar
+
+# Tech Stack
+
+- Javascript
+
+## Getting Started
+
+Install globaly with the comand
+
+> ```sh
+> npm install -g .
+> ```
+
+To run call the following command in the console (In the root of the project).
+
+> ```sh
+> nutest
+> ```
+
+- Add an array of operations per line as a string.
+- In order to finish add a blank link
---
+++ bin/index.js
@@ -0,0 +1,124 @@
+#! /usr/bin/env node
+
+// Function to print the instructions
+const printInstructions = () => {
+  console.log("Please add an array or a blank link to finish.");
+};
+
+// Function to get the inputs, calculate taxes and print the results
+const getTaxes = () => {
+  const inputArrays = new Array();
+  const readline = require("readline");
+
+  // Creating readline
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  printInstructions();
+
+  // Read the inputs
+  rl.on("line", (line) => {
+    if (line === "") {
+      // If the input is a blank line, then we can calculate the taxes
+      const taxes = calculateTaxes(inputArrays);
+      console.log("Taxes", taxes);
+      // Print the results
+      rl.write(JSON.stringify(taxes));
+      console.log("Thanks");
+      process.exit(0);
+    } else {
+      // Add input to an array and ask for more inputs
+      inputArrays.push(JSON.parse(line));
+      printInstructions();
+    }
+  });
+};
+
+// Calculte the taxes
+const calculateTaxes = (inputArray) => {
+  // Array to handle the array of results
+  const result = new Array();
+
+  // Array of inputs to be iterated
+  inputArray.forEach((operations) => {
+    // Setting the Array of taxes as result for an imput
+    const arrayOutput = new Array();
+    // Init of some variables
+    let stock = 0;
+    let loss = 0;
+    let weightedAverage = 0;
+
+    // Iterate the array of operations
+    operations.forEach((operation) => {
+      // Retrieve operation, quantity and unit cost
+      const { operation: type, quantity } = operation;
+      const unitCost = operation["unit-cost"];
+      // Getting the total of the operation
+      const operationCost = unitCost * quantity;
+
+      if (type === "buy") {
+        // If the operation is buy, then I need to calculate the weighted average price
+        weightedAverage = calculateWeightedAveragePrice(
+          stock,
+          weightedAverage,
+          quantity,
+          unitCost,
+        );
+        // Increase the stock
+        stock = stock + quantity;
+        // No taxes for buy opeartions
+        arrayOutput.push({ tax: 0.0 });
+      } else {
+        // Calculte the profit of a sell operation
+        const realCost = weightedAverage * quantity;
+        const profit = operationCost - realCost;
+
+        if (profit < 0) {
+          // If the profit is a negative number, then we have a loss
+          loss = loss + profit * -1;
+        }
+
+        if (operationCost > 20000) {
+          // Calculate the taxes and substract the loss
+          if (loss >= profit) {
+            arrayOutput.push({ tax: 0.0 });
+            if (profit > 0) {
+              loss = loss - profit;
+            }
+          } else {
+            const tax = (profit - loss) * 0.2;
+            arrayOutput.push({ tax: parseFloat(tax) });
+            loss = 0;
+          }
+        } else {
+          // If the total of the operation is less or equal than 20,000 then the taxes is 0
+          arrayOutput.push({ tax: 0.0 });
+        }
+        // Decrease the stock
+        stock = stock - quantity;
+      }
+    });
+
+    result.push(arrayOutput);
+  });
+
+  return result;
+};
+
+// Function to calculate the weighted average price
+const calculateWeightedAveragePrice = (
+  stock,
+  weightedAverage,
+  quantity,
+  unitCost,
+) => {
+  return (stock * weightedAverage + quantity * unitCost) / (stock + quantity);
+};
+
+// Welcome message
+console.log("Welcome to the code challenge, by Omar Abaroa");
+// Calling main function
+getTaxes();
